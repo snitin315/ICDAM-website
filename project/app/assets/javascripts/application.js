36f9a179ed8d458319f74eef9106706e24846e15
H-arshit <harshitsinghal33@gmail.com>
2017-09-26 23:17:17 +0530

    random commit

--- project/app/assets/javascripts/application.js
+++ project/app/assets/javascripts/application.js
@@ -13,3 +13,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 30){
+      $("body").addClass("sticky-header");
+    }else{
+      $("body").removeClass("sticky-header");
+    }//if-else
+  });//win func.
+});//ready func.
