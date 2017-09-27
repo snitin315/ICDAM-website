47c7d813902b48d7a31c0960a9fe481ec71df1b1
H-arshit <harshitsinghal33@gmail.com>
2017-09-28 01:33:18 +0530

    randomcommit

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
+  ActiveAdmin.routes(self)
   get 'home/index'
   get 'home/about_institute'
   get 'home/about_irdindia'
