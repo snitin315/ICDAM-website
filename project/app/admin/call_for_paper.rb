1d16340bed900793160fca01adaa2a798fd396fb
Sanket <singhsanket143@gmail.com>
2017-09-28 23:45:26 +0530

    latest commit

--- project/app/admin/call_for_paper.rb
+++ project/app/admin/call_for_paper.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register CallForPaper do
   # has_many :callforpapersubheadings
-has_many :callforpapersubheadings
+# has_many :callforpapersubheadings
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- project/app/admin/call_for_paper_sub_heading.rb
+++ project/app/admin/call_for_paper_sub_heading.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register CallForPaperSubHeading do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 # belongs_to :callforpaper,:parent_class =>  CallForPaper
-belongs_to :callforpaper
+# belongs_to :callforpaper
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
