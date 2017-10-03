96e4aaa4b36d3f604c78b80d89012316274be63a
pranav singhal <lucifer@shubhras-MacBook-Pro.local>
2017-10-03 14:16:35 +0530

    fixed header css

--- project/app/assets/javascripts/home.js
+++ project/app/assets/javascripts/home.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
   reset.click(function(){
     location.reload();
   })
+
+  // $(document).on('click',function(event){
+  //   console.log("herew");
+  //   var $trigger = $(".dropdown-button");
+  //   if($trigger !== event.target){
+  //     $('.dropdown-button').dropdown('close');
+  //   }
+  // })
 })
