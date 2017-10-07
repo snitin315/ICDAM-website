f7eba591fc4dbc909d1013064ebda41c30e93e97
pranav singhal <pranavsinghal96@gmail.com>
2017-10-07 23:33:02 +0530

    slimmed header

--- project/app/assets/javascripts/home.js
+++ project/app/assets/javascripts/home.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-  console.log("something");
-  console.log("something");
-  console.log("something");
-  // console.log(document.getElementById("special_submission_submit"));
+
 if (document.getElementById("special_submission_submit") !=null){
   document.getElementById("special_submission_submit").addEventListener('click',function(){
-    console.log("inside event listener");
    this.innerHTML="please wait";
   });
 }
@@ -16,8 +12,7 @@ if (document.getElementById("special_submission_submit") !=null){
   })
 if(document.getElementById("contact_us_submit")!=null){
   document.getElementById("contact_us_submit").addEventListener('click',function(){
-    console.log("inside event listener");
-  //  console.log(this);
+
    this.innerHTML="Please Wait";
 
 
