c48df418e1f51533ebd839eb5d067896a9d77d18
pranav singhal <pranavsinghal96@gmail.com>
2017-10-27 09:16:36 +0530

    added fileupload to activeadmin

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002091811) do
+ActiveRecord::Schema.define(version: 20171027024514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,13 @@ ActiveRecord::Schema.define(version: 20171002091811) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "content"
+  end
+
+  create_table "special_sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "file"
+    t.string   "string"
   end
 
   create_table "sponsors", force: :cascade do |t|
